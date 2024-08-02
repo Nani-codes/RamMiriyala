93640a93064f38a4d908fb6d21f2e74f07ee6f87
Nani <lakshminarayanamalineni1@gmail.com>
2024-08-02 18:15:07 +0530

    Navbar Fix

--- src/components/FrameComponent4.js
+++ src/components/FrameComponent4.js
@@ -17,10 +17,12 @@ const FrameComponent4 = ({ className = "" }) => {
   return (
     <div style={{
       "position":"fixed",
-      "zIndex":"99"
+      "zIndex":"99",
+      "background":"#000000",
+      "width":"100%"
     }}>
       <header
-        className={`self-stretch bg-black overflow-hidden flex flex-row items-start justify-between py-2 pr-[38px] pl-[30rem] top-[0] z-[99] sticky gap-[20px] text-left text-15xl text-red-200 font-rammetto-one border-[1px] border-solid border-black mq450:pl-5 mq450:box-border mq750:pl-[280px] mq750:box-border ${className}`}
+        className={`self-stretch bg-black overflow-hidden flex flex-row items-start justify-between py-2 pr-[38px] pl-[29rem] top-[0] z-[99] sticky gap-[20px] text-left text-15xl text-red-200 font-rammetto-one border-[1px] border-solid border-black mq450:pl-5 mq450:box-border mq750:pl-[280px] mq750:box-border ${className}`}
       >
         <div className="flex flex-col items-start justify-start pt-px px-0 pb-0">
           <a href="/"className=" [text-decoration:none] text-red-200 relative whitespace-nowrap">RAM MIRYALA</a>
